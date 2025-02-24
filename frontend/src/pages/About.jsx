8948cec2ed1b2bab57eaeb35fd81f630d62d5355
Satyam Gupta <satyamgupta9302@gmail.com>
2025-02-24 14:28:17 +0530

    change

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -12,7 +12,7 @@ const About = () => {
       <div className='my-10 flex flex-col md:flex-row gap-12'>
         <img className='w-full md:max-w-[360px]' src={assets.about_image} alt="" />
         <div className='flex flex-col justify-center gap-6 md:w-2/4 text-sm text-gray-600'>
-          <p>Welcome to Arogya, your trusted partner in managing your healthcare needs conveniently and efficiently. At Prescripto, we understand the challenges individuals face when it comes to scheduling doctor appointments and managing their health records.</p>
+          <p>Welcome to Arogya, your trusted partner in managing your healthcare needs conveniently and efficiently. At Arogya, we understand the challenges individuals face when it comes to scheduling doctor appointments and managing their health records.</p>
           <p>Arogya is committed to excellence in healthcare technology. We continuously strive to enhance our platform, integrating the latest advancements to improve user experience and deliver superior service. Whether you're booking your first appointment or managing ongoing care, Prescripto is here to support you every step of the way.</p>
           <b className='text-gray-800'>Our Vision</b>
           <p>Our vision at Arogya is to create a seamless healthcare experience for every user. We aim to bridge the gap between patients and healthcare providers, making it easier for you to access the care you need, when you need it.</p>
